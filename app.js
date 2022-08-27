c0055aab4cf34fc2a76444c938a8a02799cced60
Irina Chuprakova <mephistopheles961@gmail.com>
2022-08-27 17:11:07 +0300

    .

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const error = require('./middlewares/erros');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().requires().min(2),
+    password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(/^(ftp|http|https):\/\/[^ "]+$/),
@@ -23,7 +25,7 @@ app.post('/signup', celebrate({
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().requires().min(2),
+    password: Joi.string().required().min(2),
   }),
 }), login);
 app.use(auth);
@@ -32,5 +34,6 @@ app.use('/', require('./routes/cards'));
 
 app.use('/*', (req, res) => { res.status(404).send({ message: 'Произошла ошибка' }); });
 app.use(errors());
+app.use(error);
 
 app.listen(PORT);
